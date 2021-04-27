9c178017cd10257edde434af929508e9eeff5e1e
PoreXD <surajitpore0@gmail.com>
2021-04-27 10:37:29 +0530

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          <li>A Item </li>
-        </ul>
-      </div>
-    </div>
-  );
+    const [inputText, setInputText] = useState("");
+    const [items, setItems] = useState([]);
+
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
+
+    function addItem() {
+        setItems((prevItems) => {
+            return [...prevItems, inputText];
+        });
+        setInputText("");
+    }
+
+    return (
+        <div className="container">
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+            <div className="form">
+                <input onChange={handleChange} type="text" value={inputText} />
+                <button onClick={addItem}>
+                    <span>Add</span>
+                </button>
+            </div>
+            <div>
+                <ul>
+                    {items.map((todoItem) => (
+                        <li>{todoItem}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
